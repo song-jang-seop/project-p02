4abc9a23ec2cdfca41e10f6699449c12c1d07510
song <shoushou410@gmail.com>
2022-11-26 23:04:57 +0900

    8강 Node.js Express에서 REST API 구축

--- client/src/App.js
+++ client/src/App.js
@@ -8,34 +8,24 @@ import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import Paper from '@mui/material/Paper';
 
-const customers = [
-  {
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '홍길동1',
-  'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생'
-},
-{
- 'id': 2,
-'image': 'https://placeimg.com/64/64/2',
-'name': '홍길동2',
-'birthday': '961222',
-'gender': '남자',
-'job': '대학생'
-},
-{
-'id': 3,
-'image': 'https://placeimg.com/64/64/3',
-'name': '홍길동3',
-'birthday': '961222',
-'gender': '남자',
-'job': '대학생'
-}
-]
+class App extends Component {
 
-class App extends Component{
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     return (
       <Paper>
@@ -51,7 +41,8 @@ class App extends Component{
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
+            {this.state.customers ? this.state.customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)
+            }) : ""}
           </TableBody>
         </Table>
       </Paper>
