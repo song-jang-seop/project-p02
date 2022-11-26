534e9e6f0e91711247c355563d29a59239186cdb
song <shoushou410@gmail.com>
2022-11-26 09:45:13 +0900

    5강 고객 컴포넌트 구조화하기, 맵

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} alt="logo"/>
-      <h2>Lett's develop system</h2>
-    </div>
-  );
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '홍길동1',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+ 'id': 2,
+'image': 'https://placeimg.com/64/64/2',
+'name': '홍길동2',
+'birthday': '961222',
+'gender': '남자',
+'job': '대학생'
+},
+{
+'id': 3,
+'image': 'https://placeimg.com/64/64/3',
+'name': '홍길동3',
+'birthday': '961222',
+'gender': '남자',
+'job': '대학생'
+}
+]
+
+class App extends Component{
+  render() {
+    return (
+      <div>
+        {customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
+      </div>
+    );
+  }
 }
 
 export default App;
