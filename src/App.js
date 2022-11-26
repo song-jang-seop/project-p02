3ad5c8050ea5b31cd2ce642a7604018fcd25d8ab
song <shoushou410@gmail.com>
2022-11-26 21:19:20 +0900

    6장 Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import Customer from './components/Customer';
 import './App.css';
 import { Component } from 'react';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import Paper from '@mui/material/Paper';
 
 const customers = [
   {
@@ -32,11 +38,25 @@ const customers = [
 class App extends Component{
   render() {
     return (
-      <div>
-        {customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
-      </div>
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
-
+               
 export default App;
