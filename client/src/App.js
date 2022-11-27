e349e1a080b59f612edb0fc67a9bebd9372a5b1c
song <shoushou410@gmail.com>
2022-11-27 21:54:14 +0900

    comment

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import Customer from './components/Customer';
-import './App.css';
 import { Component } from 'react';
+import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
+import './App.css';
 import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
@@ -18,9 +19,22 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 class App extends Component {
 
-  state = {
-    customers: "",
-    completed: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0
+    }
+  }
+
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0
+    });
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
   }
 
   componentDidMount() {
@@ -43,30 +57,35 @@ class App extends Component {
 
   render() {
     return (
-      <Paper>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.customers ? this.state.customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)
-            }) : 
-            <TableRow>
-              <TableCell colSpan='6' align='center'>
-                <CircularProgress variant='determinate'value={this.state.completed}/>
-              </TableCell>
-            </TableRow>
-            }
-          </TableBody>
-        </Table>
-      </Paper>
+      <div>
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+                <TableCell>설정</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? this.state.customers.map(c => {
+                return (<Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)
+              }) : 
+              <TableRow>
+                <TableCell colSpan='6' align='center'>
+                  <CircularProgress variant='determinate'value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
+              }
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd stateRefresh={this.stateRefresh} />
+      </div>
     );
   }
 }
--- server.js
+++ server.js
@@ -21,13 +21,45 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({dest: './/upload'});
+
 app.get('/api/customers', (req, res) => {
     connection.query(
-      "select * from customer",
+      "select * from customer where isDeleted = 0",
       (err, rows, fields) => {
         res.send(rows);
       }
     );
+});
+
+app.use('/image', express.static('./upload'));
+
+//등록
+app.post('/api/customers', upload.single('image'), (req, res) => {
+  let sql = 'insert into customer values (null, ?, ?, ?, ?, ?, now(), 0)';
+  let image = '/image/' + req.file.fieldname;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  let params = [image, name, birthday, gender, job];
+  connection.query(sql, params,
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
+});
+
+//수정
+app.delete('/api/customers/:id', (req, res) => {
+  let sql = 'update customer set isDeleted = 1 where id = ?';
+  let paramms = [req.params.id];
+  connection.query(sql, paramms,
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
